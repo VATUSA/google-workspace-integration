690aed1e8775d8ad5a08b978448612f50ae58e66
Jon <jonhasacat8@gmail.com>
2023-11-09 16:31:35 -0800

    Add main functionality

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,1 +1,28 @@
 package core
+
+import (
+	"github.com/VATUSA/google-workspace-integration/internal/database"
+	"github.com/VATUSA/google-workspace-integration/internal/integration"
+)
+
+func main() {
+	err := database.Connect()
+	if err != nil {
+		return
+	}
+	err = database.MigrateDB()
+	if err != nil {
+		return
+	}
+	err = integration.CreateFacilityGroups()
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	err = integration.DoProcessStaff()
+	if err != nil {
+		println(err.Error())
+		return
+	}
+}
